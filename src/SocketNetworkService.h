e175da39ab690b6e46fb362d7b225ebabb5f77e8
Josef Karasek <josef.karasek@live.com>
2014-10-04 17:57:28 +0200

    Working on Object Composition and fighting C++ syntax instead..

--- src/SocketNetworkService.h
+++ src/SocketNetworkService.h
@@ -1,12 +1,16 @@
+#include <string>
 
 #ifndef SOCKET_NETWORK_SERVICE_H
 #define SOCKETNETWORK_SERVICE_H
 
+using namespace std;
 class SocketNetworkService {
 
 public:
     virtual ~SocketNetworkService() {};
     virtual void establishClientConnection() = 0;
+    virtual void sendMsg(string msg) = 0;
+    virtual string readMsg() = 0;
 };
 
 #endif
